7824bee894f422960317b18416efea72d12001da
aaa <135197276+aoshika02@users.noreply.github.com>
2023-10-20 11:51:17 +0900

    sanokanamu
    fade,Scene遷移

---
+++ Assets/Menber/sanokanamu/scripts/StartButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour 
+{
+    public void start_button()
+     {
+        SceneChangr.scenechangrInstance._fade.SceneFade("GameScene");
+     }
+
+ }
+
